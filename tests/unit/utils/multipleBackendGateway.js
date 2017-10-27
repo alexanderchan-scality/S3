73369c709f4f49d8e8beee6393bb6a42c815752f
Nicolas Humbert <nicolas.humbert@live.fr>
2017-10-27 13:33:49 -0700

    [review] fix

---
+++ tests/unit/utils/multipleBackendGateway.js
@@ -0,0 +1,61 @@
+const assert = require('assert');
+const { checkExternalBackend } = require('../../../lib/data/external/utils');
+// const { externalBackendHealthCheckInteval } = require('../../../constants');
+const awsLocations = [
+    'aws-test',
+];
+
+const statusSuccess = {
+    versioningStatus: 'Enabled',
+    message: 'Congrats! You own the bucket',
+};
+
+const statusFailure = {
+    versioningStatus: 'Suspended',
+    error: 'Versioning must be enabled',
+    external: true,
+};
+
+function getClients(isSuccess) {
+    const status = isSuccess ? statusSuccess : statusFailure;
+    return {
+        'aws-test': {
+            healthcheck: (location, cb) => cb(null, { 'aws-test': status }),
+        },
+    };
+}
+
+
+describe('Testing _checkExternalBackend', function describeF() {
+    this.timeout(150000);
+    beforeEach(done => {
+        const clients = getClients(true);
+        return checkExternalBackend(clients, awsLocations, 'aws_s3', done);
+    });
+    it('should not refreshed response before a minute', done => {
+        const clients = getClients(false);
+        return checkExternalBackend(clients, awsLocations, 'aws_s3',
+        (err, res) => {
+            if (err) {
+                return done(err);
+            }
+            assert.strictEqual(res['aws-test'], statusSuccess);
+            return done();
+        });
+    });
+
+    // it('should refreshed response after a minute', done => {
+    //     const clients = getClients(false);
+    //     return setTimeout(() => {
+    //         console.log('TIMEOUT!!!');
+    //         checkExternalBackend(clients, awsLocations, 'aws_s3',
+    //         (err, res) => {
+    //             if (err) {
+    //                 return done(err);
+    //             }
+    //             assert.strictEqual(res['aws-test'], statusFailure);
+    //             return done();
+    //         });
+    //     }, 50000);
+    // });
+});
